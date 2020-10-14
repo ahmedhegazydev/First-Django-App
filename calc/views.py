21ebecee428e671ded9464564e2a6dee5acd0024
Ahmed Hegazi <engahmedali2022@gmail.com>
2020-10-14 13:21:51 -0700

    Adding gitignore and README files

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+# def home(request):
+    # return HttpResponse("<h1>Hello World</h1>")
+#     return HttpResponse("Hello World")
+
+def home(request):
+    return render(request, "home.html", {'name': "Ahmed"})
+    # return render(request, "home.html")
+
